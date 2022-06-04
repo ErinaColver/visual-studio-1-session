811e157a0cb080ce8638d62a6b6e1ab3db148e3f
Steph Vargas <ceinnova06038@innovaccion.mx>
2022-06-04 12:41:22 -0500

    initial commit

---
+++ PYTHON/tiposvariables.py
@@ -0,0 +1,27 @@
+#String - Cadena de texto
+nombre = "Steph 😜"
+
+# Enteros - Números sin punto decimal
+edad = "32"
+
+print(nombre)
+
+# Flotante - Número con punto decimal
+pi = 3.1416
+
+edad = float(edad) # Casteo - transformar un tipo de variable a otra
+print(edad)
+
+# Usa Type() para saber que tipo es la variable
+print(type(nombre), type(edad))
+
+# Bool - Booleano - SÍ O NO
+legustas = False
+
+legustas = True
+
+print(legustas)
+
+
+
+
